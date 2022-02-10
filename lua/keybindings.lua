de86941403a7c17500a4c4899630e700d3db7e44
Nikolai Devolder <nikolai.devolder@gmail.com>
2022-02-10 22:20:13 +0100

    New keybindings

--- lua/keybindings.lua
+++ lua/keybindings.lua
@@ -1,13 +1,13 @@
-vim.api.nvim_set_keymap("n", "<Enter>", "o<Esc>", {})
+local opts = { noremap = true }
+local keymap = vim.api.nvim_set_keymap
 
-vim.api.nvim_set_keymap("n", "n", "nzz", {})
-vim.api.nvim_set_keymap("n", "N", "Nzz", {})
-vim.api.nvim_set_keymap("n", "*", "*zz", {})
+keymap("n", "<Enter>", "o<Esc>", opts)
 
-vim.api.nvim_set_keymap("n", "<C-PageUp>", ":bp<CR>", {})
-vim.api.nvim_set_keymap("n", "<C-PageDown>", ":bn<CR>", {})
+keymap("n", "n", "nzz", opts)
+keymap("n", "N", "Nzz", opts)
+keymap("n", "*", "*zz", opts)
 
-vim.api.nvim_set_keymap("n", "<F5>", ":setlocal spell! spelllang=en_us<CR>", {})
-vim.api.nvim_set_keymap("n", "<F6>", ":setlocal spell! spelllang=nl<CR>", {})
+keymap("n", "<C-PageUp>", ":bp<CR>", opts)
+keymap("n", "<C-PageDown>", ":bn<CR>", opts)
 
 vim.api.nvim_set_keymap("v", "//", "y/\\V<C-R>=escape(@\",\'/\\\')<CR><CR>", {})
