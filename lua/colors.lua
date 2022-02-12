9ae4646701471514129e52a55f8096a7959df155
Nikolai Devolder <nikolai.devolder@gmail.com>
2022-02-12 12:03:27 +0100

    Error prone colors

--- lua/colors.lua
+++ lua/colors.lua
@@ -1,3 +1,7 @@
-vim.o.termguicolors = true
-vim.cmd 'syntax enable'
-vim.cmd 'colorscheme dracula'
+local colorscheme = "dracula"
+
+local status_ok, _ = pcall(vim.cmd, "colorscheme " .. colorscheme)
+if not status_ok then
+  vim.notify("colorscheme " .. colorscheme .. " not found!")
+  return
+end
