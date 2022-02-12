5bf4708ef313fb1c4dd762349a30c19af988dec4
Nikolai Devolder <nikolai.devolder@gmail.com>
2022-02-12 18:25:53 +0100

    Treesitter working pcall

--- lua/user/treesitter.lua
+++ lua/user/treesitter.lua
@@ -1,11 +1,15 @@
-status_ok, treesitter = pcall('require', 'nvim-treesitter.configs')
+require'nvim-treesitter.configs'
+local status_ok, treesitter = pcall(require, 'nvim-treesitter.configs')
 if not status_ok then
+    print'Error! No Treesitter'
     return;
 end
 
 -- Install with TSInstall <name_lang>
 treesitter.setup {
+    ensure_installed = "maintained",
     highlight = {
         enable = true,
+        additional_vim_regex_highlighting = false,
     },
 }
