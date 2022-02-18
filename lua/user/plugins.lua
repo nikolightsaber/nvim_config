f2d35bbfa8e8d8bb22bf1dff88d38059d9c49220
Nikolai Devolder <nikolai.devolder@gmail.com>
2022-02-18 23:05:32 +0100

    Fix playround commit because they break stuff

--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -44,7 +44,7 @@ packer.startup(function()
 
     use { 'nvim-treesitter/nvim-treesitter', run = ':TSUpdate' }
     use "p00f/nvim-ts-rainbow"
-    use 'nvim-treesitter/playground'
+    use { 'nvim-treesitter/playground', commit = "787a7a8d4444e58467d6b3d4b88a497e7d494643" }
 
     use "Mofiqul/dracula.nvim"
 
