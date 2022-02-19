7a5822c46bf7cf9eccc1a91e18805fdd3075f084
Nikolai Devolder <nikolai.devolder@gmail.com>
2022-02-19 11:22:26 +0100

    lualine more info

--- lua/user/lualine.lua
+++ lua/user/lualine.lua
@@ -4,6 +4,43 @@ if not status_ok then
     return;
 end
 
+local hide_in_width = function()
+	return vim.fn.winwidth(0) > 80
+end
+
+local diagnostics = {
+	"diagnostics",
+	sources = { "nvim_diagnostic" },
+	sections = { "error", "warn" },
+	symbols = { error = " ", warn = " " },
+	colored = false,
+	update_in_insert = false,
+	always_visible = true,
+}
+
+local diff = {
+	"diff",
+	colored = false,
+	symbols = { added = " ", modified = " ", removed = " " }, -- changes diff symbols
+    cond = hide_in_width
+}
+
+local branch = {
+	"branch",
+	icons_enabled = true,
+	icon = "",
+}
+
+local filetype = {
+	"filetype",
+	icons_enabled = false,
+	icon = nil,
+}
+
+local spaces = function()
+	return "spaces: " .. vim.api.nvim_buf_get_option(0, "shiftwidth")
+end
+
 lualine.setup({
   options = {
     icons_enabled = true,
@@ -15,9 +52,10 @@ lualine.setup({
   },
   sections = {
     lualine_a = {'mode'},
-    lualine_b = {'branch', 'diff', 'diagnostics'},
+    lualine_b = { branch, diagnostics},
     lualine_c = { {'filename', file_status = true, path = 1} },
-    lualine_x = {'encoding', 'fileformat', 'filetype'},
+    -- lualine_x = {'encoding', 'fileformat', 'filetype'},
+    lualine_x = { diff, spaces, "encoding", filetype },
     lualine_y = {'progress'},
     lualine_z = {'location'}
   },
