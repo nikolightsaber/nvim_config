762423ee4017f65746f06bc4f568886697b7aa4c
Nikolai Devolder <nikolai.devolder@gmail.com>
2022-03-10 20:29:58 +0100

    angularls temp update

--- lua/user/lsp/settings/angularls.lua
+++ lua/user/lsp/settings/angularls.lua
@@ -1,7 +1,3 @@
-local cmd = {"ngserver", "--stdio", "--tsProbeLocations", vim.fn.getcwd() .. "/node_modules" , "--ngProbeLocations", vim.fn.getcwd() .. "/node_modules"}
 return {
-  cmd = cmd,
-  on_new_config = function(new_config,new_root_dir)
-    new_config.cmd = cmd
-  end,
+
 }
