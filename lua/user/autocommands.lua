1e570d670cbd18e91ee49f98bae2c9cdf00d5bb1
Nikolai Devolder <nikolai.devolder@yamabiko.eu>
2022-04-15 08:59:59 +0200

    Trailing white space highlight

--- lua/user/autocommands.lua
+++ lua/user/autocommands.lua
@@ -15,4 +15,14 @@ vim.cmd [[
     autocmd FileType markdown setlocal wrap
     autocmd FileType markdown setlocal spell
   augroup end
+
+  highlight ExtraWhitespace ctermbg=darkgreen guibg=darkgreen
+  autocmd ColorScheme * highlight ExtraWhitespace ctermbg=darkgreen guibg=darkgreen
+  " Show trailing whitespace and spaces before a tab:
+  match ExtraWhitespace /\s\+$\| \+\ze\t/
+  " Show tabs that are not at the start of a line:
+  match ExtraWhitespace /[^\t]\zs\t\+/
+  " Show trailing whitespaces when not in insert mode:
+  au InsertEnter * match ExtraWhitespace /\s\+\%#\@<!$/
+  au InsertLeave * match ExtraWhitespace /\s\+$/
 ]]
