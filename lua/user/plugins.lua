ee30ddbda4be0818ead9b2bf76e436d525200658
Nikolai Devolder <nikolai.devolder@gmail.com>
2022-05-19 19:56:32 +0200

    whichkey added and config

--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -81,6 +81,8 @@ packer.startup(function()
 
   use 'SmiteshP/nvim-gps'
 
+  use "folke/which-key.nvim"
+
   -- Automatically set up your configuration after cloning packer.nvim
   -- Put this at the end after all plugins
   if PACKER_BOOTSTRAP then
