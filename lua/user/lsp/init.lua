902c4b0e8da388078a6f45273755611a2c11e419
Nikolai Devolder <nikolai.devolder@yamabiko.eu>
2022-09-02 17:42:25 +0200

    Rename settings

--- lua/user/lsp/init.lua
+++ lua/user/lsp/init.lua
@@ -1,8 +1,2 @@
-local status_ok, _ = pcall(require, "lspconfig")
-if not status_ok then
-  print("lsp not ok")
-  return
-end
-
-require("user.lsp.lsp-installer")
+require("user.lsp.setup")
 require("user.lsp.handlers").setup()
