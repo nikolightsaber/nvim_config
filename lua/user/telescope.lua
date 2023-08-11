a78806576ccaecaff748320776bcd8c029532a0d
Nikolai Devolder <nikolai.devolder@yamabiko.eu>
2023-08-11 09:04:02 +0200

    Add dupnavi for no ignore

--- lua/user/telescope.lua
+++ lua/user/telescope.lua
@@ -125,7 +125,7 @@ telescope.setup(setup)
 local files = function()
   local current_repo = require("user.utils").current_repo()
   local opts = { previewer = false, path_display = { "absolute" }, no_ignore = false }
-  if current_repo == "navigation" then
+  if current_repo == "navigation" or current_repo == "dupnavi" then
     opts.no_ignore = true
   end
   return builtin.find_files(opts)
