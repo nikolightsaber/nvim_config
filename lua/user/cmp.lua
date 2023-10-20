ad39d1ebf9cf3adacc11cb116efdc6ed09b79293
Nikolai Devolder <nikolai.devolder@yamabiko.eu>
2023-10-20 14:23:01 +0200

    reduce cmp complexity

--- lua/user/cmp.lua
+++ lua/user/cmp.lua
@@ -68,24 +68,6 @@ cmp.setup {
     ["<Tab>"] = cmp.mapping(function(fallback)
       if cmp.visible() then
         cmp.select_next_item()
-      elseif luasnip.expandable() then
-        luasnip.expand({})
-      elseif luasnip.expand_or_jumpable() then
-        luasnip.expand_or_jump()
-      elseif check_backspace() then
-        fallback()
-      else
-        fallback()
-      end
-    end, {
-        "i",
-        "s",
-      }),
-    ["<S-Tab>"] = cmp.mapping(function(fallback)
-      if cmp.visible() then
-        cmp.select_prev_item()
-      elseif luasnip.jumpable(-1) then
-        luasnip.jump(-1)
       else
         fallback()
       end
@@ -115,7 +97,7 @@ cmp.setup {
   { name = "nvim_lua" },
   { name = "luasnip" },
   { name = "buffer" },
-  { name = "path" },
+  -- { name = "path" },
   { name = 'nvim_lsp_signature_help' }
   },
   confirm_opts = {
@@ -126,8 +108,5 @@ cmp.setup {
     documentation = {
       border = { "╭", "─", "╮", "│", "╯", "─", "╰", "│" },
     }
-  },
-  experimental = {
-    ghost_text = true,
   },
 }
