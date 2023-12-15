1eda47f7954a5f27674912e6383c38f859bb7490
Nikolai Devolder <nikolai.devolder@yamabiko.eu>
2023-12-15 16:42:24 +0100

    harpoon c-h

--- lua/user/harpoon.lua
+++ lua/user/harpoon.lua
@@ -7,7 +7,7 @@ end
 harpoon:setup()
 
 vim.keymap.set("n", "<leader>a", function() harpoon:list():append() end)
-vim.keymap.set("n", "<C-e>", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end)
+vim.keymap.set("n", "<C-h>", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end)
 
 vim.keymap.set("n", "<C-j>", function() harpoon:list():select(1) end)
 vim.keymap.set("n", "<C-k>", function() harpoon:list():select(2) end)
