55ad53cd93e3b79562c25a6dbf4528f1dba5e5b1
Nikolai Devolder <nikolai.devolder@yamabiko.eu>
2024-01-17 16:41:58 +0100

    Move old to config file

--- lua/user_old/colors.lua
+++ lua/user_old/colors.lua
@@ -2,13 +2,9 @@ local colorscheme = "dracula"
 -- colorscheme = "tokyonight"
 -- colorscheme = "tokyodark"
 colorscheme = "gruvbox-baby"
--- local status_ok, rose_pine = pcall(require, "rose-pine")
--- if status_ok then
---   rose_pine.setup({ dark_variant = "moon" })
--- end
---colorscheme = "rose-pine"
-vim.g.gruvbox_baby_background_color = "dark"
-vim.g.gruvbox_baby_highlights = {Search = {fg = "Black", bg = "Orange", style="underline"}}
+colorscheme = "rose-pine-moon"
+-- vim.g.gruvbox_baby_background_color = "dark"
+-- vim.g.gruvbox_baby_highlights = {Search = {fg = "Black", bg = "Orange", style="underline"}}
 
 local status_ok, _ = pcall(vim.cmd, "colorscheme " .. colorscheme)
 if not status_ok then
---
+++ lua/user_old/lualine.lua
@@ -0,0 +1,71 @@
+local status_ok, lualine = pcall(require, 'lualine')
+if not status_ok then
+  print('Error! No lualine')
+  return;
+end
+
+local hide_in_width = function()
+  return vim.fn.winwidth(0) > 80
+end
+
+-- local diagnostics = {
+--   "diagnostics",
+--   sources = { "nvim_diagnostic" },
+--   sections = { "error", "warn" },
+--   symbols = { error = " ", warn = " " },
+--   colored = false,
+--   update_in_insert = false,
+--   always_visible = true,
+-- }
+
+local diff = {
+  "diff",
+  colored = false,
+  symbols = { added = " ", modified = " ", removed = " " }, -- changes diff symbols
+  cond = hide_in_width
+}
+
+local branch = {
+  "branch",
+  icons_enabled = true,
+  icon = "",
+}
+
+local filetype = {
+  "filetype",
+  icons_enabled = false,
+  icon = nil,
+}
+
+local spaces = function()
+  return "spaces: " .. vim.api.nvim_buf_get_option(0, "shiftwidth")
+end
+
+lualine.setup({
+  options = {
+    icons_enabled = true,
+    theme = 'auto',
+    component_separators = { left = '', right = '' },
+    section_separators = { left = '', right = '' },
+    disabled_filetypes = { "dashboard", "NvimTree", "Outline" },
+    always_divide_middle = true,
+  },
+  sections = {
+    lualine_a = { { 'mode', fmt = function(str) return str:sub(1,1) end } },
+    lualine_b = { branch },
+    lualine_c = { { 'filename', file_status = true, path = 1 } },
+    lualine_x = { diff, spaces, "encoding", filetype },
+    lualine_y = { 'progress' },
+    lualine_z = { 'location' }
+  },
+  inactive_sections = {
+    lualine_a = {},
+    lualine_b = {},
+    lualine_c = { { 'filename', file_status = true, path = 1 } },
+    lualine_x = { 'location' },
+    lualine_y = {},
+    lualine_z = {}
+  },
+  tabline = {},
+  extensions = {}
+})
