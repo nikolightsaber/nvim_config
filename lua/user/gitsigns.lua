931d13e761e6f964935114700c9386ca9ce4075e
Nikolai Devolder <nikolai.devolder@yamabiko.eu>
2024-01-29 09:58:10 +0100

    Gitsigns better hl

--- lua/user/gitsigns.lua
+++ lua/user/gitsigns.lua
@@ -4,7 +4,11 @@ return {
   config = function ()
     require("gitsigns").setup({
       signs = {
-        delete = { hl = "GitSignsDelete", text = "契", numhl = "GitSignsDeleteNr", linehl = "GitSignsDeleteLn" },
+    add = { hl = "GitSignsAdd", text = "▎", numhl = "GitSignsAddNr", linehl = "GitSignsAddLn" },
+    change = { hl = "GitSignsChange", text = "▎", numhl = "GitSignsChangeNr", linehl = "GitSignsChangeLn" },
+    delete = { hl = "GitSignsDelete", text = "契", numhl = "GitSignsDeleteNr", linehl = "GitSignsDeleteLn" },
+    topdelete = { hl = "GitSignsDelete", text = "契", numhl = "GitSignsDeleteNr", linehl = "GitSignsDeleteLn" },
+    changedelete = { hl = "GitSignsChange", text = "▎", numhl = "GitSignsChangeNr", linehl = "GitSignsChangeLn" },
       },
       current_line_blame_opts = {
         virt_text = true,
