e17035f400914264b6c034e5b6d856eb41098e90
Nikolai Devolder <nikolai.devolder@yamabiko.eu>
2024-02-05 09:34:48 +0100

    Fix snippets add cmp snippet source

--- lua/user/cmp.lua
+++ lua/user/cmp.lua
@@ -3,6 +3,7 @@ return {
   event = "VeryLazy",
   dependencies = {
     "L3MON4D3/LuaSnip",
+    "saadparwaiz1/cmp_luasnip",
     "hrsh7th/cmp-nvim-lsp",
     "hrsh7th/cmp-path",
     "hrsh7th/cmp-cmdline",
