9d80c8fe41fc1a40e8adf32187c8d34fa8cdfb67
Nikolai Devolder <nikolai.devolder@yamabiko.eu>
2024-02-15 15:24:43 +0100

    blame diff if possible

--- lua/user/gitsigns.lua
+++ lua/user/gitsigns.lua
@@ -1,14 +1,15 @@
 return {
   "lewis6991/gitsigns.nvim",
   event = "VeryLazy",
-  config = function ()
-    require("gitsigns").setup({
+  config = function()
+    local gitsigns = require("gitsigns")
+    gitsigns.setup({
       signs = {
-    add = { hl = "GitSignsAdd", text = "▎", numhl = "GitSignsAddNr", linehl = "GitSignsAddLn" },
-    change = { hl = "GitSignsChange", text = "▎", numhl = "GitSignsChangeNr", linehl = "GitSignsChangeLn" },
-    delete = { hl = "GitSignsDelete", text = "契", numhl = "GitSignsDeleteNr", linehl = "GitSignsDeleteLn" },
-    topdelete = { hl = "GitSignsDelete", text = "契", numhl = "GitSignsDeleteNr", linehl = "GitSignsDeleteLn" },
-    changedelete = { hl = "GitSignsChange", text = "▎", numhl = "GitSignsChangeNr", linehl = "GitSignsChangeLn" },
+        add = { hl = "GitSignsAdd", text = "▎", numhl = "GitSignsAddNr", linehl = "GitSignsAddLn" },
+        change = { hl = "GitSignsChange", text = "▎", numhl = "GitSignsChangeNr", linehl = "GitSignsChangeLn" },
+        delete = { hl = "GitSignsDelete", text = "契", numhl = "GitSignsDeleteNr", linehl = "GitSignsDeleteLn" },
+        topdelete = { hl = "GitSignsDelete", text = "契", numhl = "GitSignsDeleteNr", linehl = "GitSignsDeleteLn" },
+        changedelete = { hl = "GitSignsChange", text = "▎", numhl = "GitSignsChangeNr", linehl = "GitSignsChangeLn" },
       },
       current_line_blame_opts = {
         virt_text = true,
@@ -17,11 +18,25 @@ return {
         ignore_whitespace = false,
       },
     })
-    vim.keymap.set("n", "<leader>w", function () require('gitsigns.actions').blame_line({ full = true, ignore_whitespace = true }) end)
-    vim.keymap.set("n", "<leader>ts", require('gitsigns.actions').toggle_signs )
-    vim.api.nvim_set_hl(0, "GitSignsCurrentLineBlame", { fg="#6c7993" })
-    vim.keymap.set("n", "<leader>tb", require('gitsigns.actions').toggle_current_line_blame )
-    vim.keymap.set("n", "<leader>jh", require('gitsigns.actions').next_hunk )
-    vim.keymap.set("n", "<leader>kh", require('gitsigns.actions').prev_hunk )
+
+    local blame_or_preview = function()
+      local actions = gitsigns.get_actions()
+      local blame_line = actions["blame_line"]
+      if (blame_line ~= nil) then
+        blame_line({ full = true, ignore_whitespace = true })
+      end
+
+      local preview_hunk = actions["preview_hunk"]
+      if (preview_hunk ~= nil) then
+        preview_hunk({ full = true, ignore_whitespace = true })
+      end
+    end
+
+    vim.keymap.set("n", "<leader>w", blame_or_preview)
+    vim.keymap.set("n", "<leader>ts", require('gitsigns.actions').toggle_signs)
+    vim.api.nvim_set_hl(0, "GitSignsCurrentLineBlame", { fg = "#6c7993" })
+    vim.keymap.set("n", "<leader>tb", require('gitsigns.actions').toggle_current_line_blame)
+    vim.keymap.set("n", "<leader>jh", require('gitsigns.actions').next_hunk)
+    vim.keymap.set("n", "<leader>kh", require('gitsigns.actions').prev_hunk)
   end
 }
