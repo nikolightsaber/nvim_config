6440fcc72e9fa107dd0a9f5aabb594afc094dcd6
Nikolai Devolder <nikolai.devolder@yamabiko.eu>
2024-03-22 16:10:06 +0100

    filetype icon only

--- lua/user/lualine.lua
+++ lua/user/lualine.lua
@@ -7,8 +7,11 @@ return {
       lualine_a = { { 'mode', fmt = function(str) return str:sub(1, 1) end } },
       lualine_b = { { "branch", icons_enabled = true, icon = "", } },
       lualine_c = { { 'filename', file_status = true, path = 1 } },
-      lualine_x = { function() return "spaces: " .. vim.api.nvim_get_option_value("shiftwidth", {}) end, "encoding",
-        "filetype" },
+      lualine_x = {
+        function() return "spaces: " .. vim.api.nvim_get_option_value("shiftwidth", {}) end,
+        "encoding",
+        { "filetype", icon_only = true, }
+      },
       lualine_y = { 'progress' },
       lualine_z = { 'location' }
     },
