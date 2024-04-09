0a5ee2af77f414b44a6e7a4123cb0a6ffcd242a8
Nikolai Devolder <nikolai.devolder@yamabiko.eu>
2024-04-09 13:27:11 +0200

    Harpoon append deprecated

--- lua/user/harpoon.lua
+++ lua/user/harpoon.lua
@@ -19,7 +19,7 @@ return {
       }
     })
 
-    vim.keymap.set("n", "<leader>a", function() harpoon:list():append() end)
+    vim.keymap.set("n", "<leader>a", function() harpoon:list():add() end)
     vim.keymap.set("n", "<C-h>", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end)
 
     vim.keymap.set("n", "<C-j>", function() harpoon:list():select(1) end)
