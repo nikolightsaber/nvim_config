ecb61e55f63656cee139725987a165492f1d22ce
Nikolai Devolder <nikolai.devolder@yamabiko.eu>
2024-05-21 09:25:30 +0200

    Fix update nvim 0.10

--- lua/user/lsp/init.lua
+++ lua/user/lsp/init.lua
@@ -12,24 +12,11 @@ return {
     end
     require("user.lsp.servers")
 
-    local signs = {
-      { name = "DiagnosticSignError", text = "" },
-      { name = "DiagnosticSignWarn", text = "" },
-      { name = "DiagnosticSignHint", text = "" },
-      { name = "DiagnosticSignInfo", text = "" },
-    }
-
-    for _, sign in ipairs(signs) do
-      vim.fn.sign_define(sign.name, { texthl = sign.name, text = sign.text, numhl = "" })
-    end
-
     local config = {
       virtual_text = false,
-      signs = {
-        active = signs,
-      },
       update_in_insert = true,
       underline = true,
+
       severity_sort = true,
       float = {
         focusable = false,
@@ -39,6 +26,14 @@ return {
         header = "",
         prefix = "",
       },
+      signs = {
+        text = {
+          [vim.diagnostic.severity.INFO] = "",
+          [vim.diagnostic.severity.HINT] = "",
+          [vim.diagnostic.severity.WARN] = "",
+          [vim.diagnostic.severity.ERROR] = "",
+        }
+      }
     }
 
     vim.diagnostic.config(config)
