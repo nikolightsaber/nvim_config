5a34dbcfaffd0df2c0a48f0a7fb5f78db980b372
Nikolai Devolder <nikolai.devolder@yamabiko.eu>
2024-06-26 09:25:39 +0200

    Remove snippets

--- lua/user/cmp.lua
+++ lua/user/cmp.lua
@@ -1,16 +1,12 @@
 return {
   "hrsh7th/nvim-cmp",
-  event = "VeryLazy",
+  event = "InsertEnter",
   dependencies = {
-    "L3MON4D3/LuaSnip",
-    "saadparwaiz1/cmp_luasnip",
     "hrsh7th/cmp-nvim-lsp",
     "hrsh7th/cmp-buffer",
     "hrsh7th/cmp-nvim-lsp-signature-help",
-    "rafamadriz/friendly-snippets",
   },
   config = function()
-    --   פּ ﯟ   some other good icons
     local kind_icons = {
       Text = "",
       Method = "",
@@ -40,42 +36,20 @@ return {
     }
 
     local cmp = require("cmp")
-    local luasnip = require("luasnip")
-    require("luasnip/loaders/from_vscode").lazy_load()
     cmp.setup({
-      snippet = {
-        expand = function(args)
-          luasnip.lsp_expand(args.body)
-        end,
-      },
       mapping = cmp.mapping.preset.insert({
         ['<C-n>'] = cmp.mapping.select_next_item(),
         ['<C-p>'] = cmp.mapping.select_prev_item(),
-
         ['<C-y>'] = cmp.mapping.confirm { select = true },
-
         ['<C-Space>'] = cmp.mapping.complete {},
-        ['<C-l>'] = cmp.mapping(function()
-          if luasnip.expand_or_locally_jumpable() then
-            luasnip.expand_or_jump()
-          end
-        end, { 'i', 's' }),
-        ['<C-h>'] = cmp.mapping(function()
-          if luasnip.locally_jumpable(-1) then
-            luasnip.jump(-1)
-          end
-        end, { 'i', 's' }),
       }),
       ---@diagnostic disable-next-line: missing-fields
       formatting = {
         fields = { "kind", "abbr", "menu" },
         format = function(entry, vim_item)
-          -- Kind icons
           vim_item.kind = string.format("%s", kind_icons[vim_item.kind])
-          -- vim_item.kind = string.format('%s %s', kind_icons[vim_item.kind], vim_item.kind) -- This concatonates the icons with the name of the item kind
           vim_item.menu = ({
             nvim_lsp = "[LSP]",
-            luasnip = "[Snippet]",
             buffer = "[Buffer]",
           })[entry.source.name]
           return vim_item
@@ -83,7 +57,6 @@ return {
       },
       sources = {
         { name = "nvim_lsp" },
-        { name = "luasnip" },
         {
           name = "buffer",
           max_item_count = 5,
