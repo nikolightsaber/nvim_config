54831996ee51b8d433168ece324f876a8bdef49b
Nikolai Devolder <nikolai.devolder@yamabiko.eu>
2024-09-13 08:56:46 +0200

    Extract harpoon

--- lua/user/lazy.lua
+++ lua/user/lazy.lua
@@ -43,7 +43,26 @@ require("lazy").setup({
       vim.cmd.colorscheme("tokyonight-storm")
     end,
   },
-  require("user.harpoon"),
+  {
+    "ThePrimeagen/harpoon",
+    branch = "harpoon2",
+    dependencies = { "nvim-lua/plenary.nvim" },
+    keys = { "<leader>a", "<C-h>", "<C-j>", "<C-k>", "<C-l>", "<C-;>", "<C-'>", "<C-\\>" },
+    config = function()
+      local harpoon = require("harpoon")
+      harpoon:setup({ settings = { save_on_toggle = true, } })
+
+      vim.keymap.set("n", "<leader>a", function() harpoon:list():add() end)
+      vim.keymap.set("n", "<C-h>", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end)
+
+      vim.keymap.set("n", "<C-j>", function() harpoon:list():select(1) end)
+      vim.keymap.set("n", "<C-k>", function() harpoon:list():select(2) end)
+      vim.keymap.set("n", "<C-l>", function() harpoon:list():select(3) end)
+      vim.keymap.set("n", "<C-;>", function() harpoon:list():select(4) end)
+      vim.keymap.set("n", "<C-'>", function() harpoon:list():select(5) end)
+      vim.keymap.set("n", "<C-\\>", function() harpoon:list():select(6) end)
+    end,
+  },
   require("user.telescope"),
   require("user.gitsigns"),
   {
