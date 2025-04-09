919552e6c4f8a2934be0617985391848101b507d
Nikolai Devolder <nikolai.devolder@yamabiko.eu>
2025-04-09 22:00:05 +0200

    remove diagnositic ignore on vim.uv

--- lua/user/lsp/servers.lua
+++ lua/user/lsp/servers.lua
@@ -16,7 +16,6 @@ local lspconfig = require("lspconfig")
 local lua_ls_opts = {
   on_init = function(client)
     local path = client.workspace_folders[1].name
-    ---@diagnostic disable-next-line: undefined-field
     if not vim.uv.fs_stat(path .. "/.luarc.json") and not vim.uv.fs_stat(path .. "/.luarc.jsonc") then
       client.config.settings = vim.tbl_deep_extend("force", client.config.settings, {
         Lua = {
