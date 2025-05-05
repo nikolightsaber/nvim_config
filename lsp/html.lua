841310b801b6380d6bc985462d33720e846b47db
Nikolai Devolder <nikolai.devolder@yamabiko.eu>
2025-05-05 20:15:00 +0200

    Removed unused html lsp setting

--- lsp/html.lua
+++ lsp/html.lua
@@ -6,7 +6,6 @@ return {
   cmd = { 'vscode-html-language-server', '--stdio' },
   filetypes = { 'html', 'templ' },
   root_markers = { 'package.json', '.git' },
-  settings = {},
   init_options = {
     provideFormatter = true,
     embeddedLanguages = { css = true, javascript = true },
