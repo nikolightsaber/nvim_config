ed6a6b7a4bde69abe480eef895a137f8b345cbd0
Nikolai Devolder <nikolai.devolder@yamabiko.eu>
2025-10-14 08:28:46 +0200

    Add shortcut for undotree

--- lua/user/pack.lua
+++ lua/user/pack.lua
@@ -15,6 +15,8 @@ vim.cmd.colorscheme('tokyonight-storm')
 vim.cmd.packadd('nvim.difftool')
 vim.cmd.packadd('nvim.undotree')
 
+vim.keymap.set('n', '<leader>u', require('undotree').open);
+
 require('telescope').setup({
   defaults = {
     mappings = {
