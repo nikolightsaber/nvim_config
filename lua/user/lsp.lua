b1c653cbf135bf37c15d5f8333decb654e01ca06
Nikolai Devolder <nikolai.devolder@yamabiko.eu>
2026-05-05 18:02:49 +0200

    Add lsp progress

--- lua/user/lsp.lua
+++ lua/user/lsp.lua
@@ -111,6 +111,21 @@ local on_attach = function(client, bufnr)
   lsp_keymaps(bufnr)
   lsp_highlight_document(client, bufnr)
   lsp_completion_info(client, bufnr)
+
+  vim.api.nvim_create_autocmd('LspProgress', {
+    buf = bufnr,
+    callback = function(ev)
+      local value = ev.data.params.value
+      vim.api.nvim_echo({ { value.message or 'done' } }, false, {
+        id = 'lsp.' .. ev.data.client_id,
+        kind = 'progress',
+        source = 'vim.lsp',
+        title = value.title,
+        status = value.kind ~= 'end' and 'running' or 'success',
+        percent = value.percentage,
+      })
+    end,
+  })
 end
 
 --- @param client (vim.lsp.Client)
